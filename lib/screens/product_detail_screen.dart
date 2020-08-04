edda0d8bd61ff73f2fd154ba6453377c17279eb5
Amit Patil <amitgpatil215@gmail.com>
2020-08-04 21:14:02 +0530

    Filtering on basis of id in provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/Product-Detail';
@@ -6,9 +8,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context).settings.arguments;
+
+    // Getting details of particular id
+    //accessing provider of products having getter items by filtering out with matching id
+    final loadedProduct = Provider.of<Products>(context).findById(id);
     return Scaffold(
       appBar: AppBar(
-        title: Text("title"),
+        title: Text(loadedProduct.title),
       ),
     );
   }
