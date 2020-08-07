cc702ff511b2478309524e850bafe590bc221ac7
Amit Patil <amitgpatil215@gmail.com>
2020-08-07 20:50:04 +0530

    Displaying a List of Cart Items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/cart.dart';
+
+// ! cartItem Clashing in both items
+//  as we only need Cart for our provider
+//  so using show key word we can acess to Cart only
+
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routName = '/cart';
@@ -42,6 +48,27 @@ class CartScreen extends StatelessWidget {
                   )
                 ],
               ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // ! as Listview can be assigned in Column
+          // using Expanded widget
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.cartItemCount,
+              itemBuilder: (ctx, index) {
+                return CartItem(
+                  // * WE TRYING TO ACESS MAP items
+                  // * so first getting its value and then covertingit to list
+                  // * there after acessing with each item indec
+                  id: cart.items.values.toList()[index].id,
+                  quantity: cart.items.values.toList()[index].quantity,
+                  price: cart.items.values.toList()[index].price,
+                  title: cart.items.values.toList()[index].title,
+                );
+              },
             ),
           )
         ],
