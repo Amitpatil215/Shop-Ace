d788549df318e9628c1c5ca897c069625cdbca7c
Amit Patil <amitgpatil215@gmail.com>
2020-08-09 13:00:37 +0530

    Making Order Expandable using Stateful Widgets instead of Providers

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,14 +1,22 @@
+import 'dart:math'; // for using min function
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 // * We have widget class with Name of OrderItem in
 // providers/orders.dart & widget/order_item.dart so using ord
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   //acessing OrderItem from 'ord' dile
   final ord.OrderItem order;
 
   const OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +25,60 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text("\$${order.amount}"),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
-            trailing:
-                IconButton(icon: Icon(Icons.expand_more), onPressed: () {}),
-          )
+            title: Text("\$${widget.order.amount}"),
+            subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+            trailing: IconButton(
+              icon: Icon(
+                _expanded ? Icons.expand_less : Icons.expand_more,
+              ),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          // we can use if() in a widget list
+          if (_expanded)
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: min(widget.order.products.length * 20.0 + 10, 100),
+              child: ListView(
+                children: widget.order.products.map((eachProduct) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        eachProduct.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        "${eachProduct.quantity}X  ",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        "\$${eachProduct.price}",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
         ],
       ),
     );
