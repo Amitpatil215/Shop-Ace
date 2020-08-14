6c935f74909299ba4fe7a91f4c4d37eb8a8c5fec
Amit Patil <amitgpatil215@gmail.com>
2020-08-14 11:22:53 +0530

    Using FutureBuilder widget instead of statefull widget

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,20 +4,16 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routName = '/orders';
 
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
+  // var _isLoading = false;
 
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
+/*
   @override
   void initState() {
-    //as we cant use future in init state
-    //using special function future delayed with value of .zero duration
+    /// as we cant use future in init state
+    /// using special function future delayed with value of .zero duration
     Future.delayed(Duration.zero).then((value) async {
       setState(() {
         _isLoading = true;
@@ -30,25 +26,45 @@ class _OrdersScreenState extends State<OrdersScreen> {
 
     super.initState();
   }
-
+*/
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Orders!"),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
+      // * Using FutureBuilder instead of seeting state of _isLoading
+      // so just for fetching data we dont need to make it statefull
+      // it also avoid rebuilding of build method just cause of _isLoading
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, currentDataSnapshot) {
+          if (currentDataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, index) {
-                return OrderItem(orderData.orders[index]);
-              },
-            ),
+            );
+          }
+          if (currentDataSnapshot.error != null) {
+            // do error handling here
+            return Center(
+              child: Text("error Ocuured"),
+            );
+          }
+          // if (currentDataSnapshot.connectionState == ConnectionState.done)
+          else {
+            return Consumer<Orders>(
+              builder: (ctx, orderData, child) => ListView.builder(
+                itemCount: orderData.orders.length,
+                itemBuilder: (ctx, index) {
+                  return OrderItem(orderData.orders[index]);
+                },
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
